655406664176a8953a081fc9178c06067e481a31
Ami13F <ameliaforgacs@gmail.com>
2022-01-19 11:33:11 +0200

    [fix] Use React.Fragment to generate less spans

--- react-training/src/components/Highlighted.tsx
+++ react-training/src/components/Highlighted.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Highlighted = ({ text = "", highlight = "" }) => {
   highlight = highlight.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
   if (!highlight.trim()) {
@@ -6,11 +8,14 @@ const Highlighted = ({ text = "", highlight = "" }) => {
   const regex = new RegExp(`(${[highlight]})`, "gi");
   const parts = text.split(regex);
   return (
-    <span>
+    <>
       {parts
         .filter(part => part)
-        .map((part, i) => (regex.test(part) ? <mark key={i}>{part}</mark> : <span key={i}>{part}</span>))}
-    </span>
+        .map(
+          (part, i) =>
+            regex.test(part) ? <mark key={i}>{part}</mark> : <React.Fragment key={i}>{part}</React.Fragment> // React.Fragmet allow you to create an empty tag but also use tags(like key)
+        )}
+    </>
   );
 };
 
