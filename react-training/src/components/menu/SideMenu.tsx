4c13edeae4c779df95c988c7c7b98f3671761311
Ami13F <ameliaforgacs@gmail.com>
2022-01-20 13:01:28 +0200

    [impl] Use todo app as side menu

--- react-training/src/components/menu/SideMenu.tsx
+++ react-training/src/components/menu/SideMenu.tsx
@@ -1,8 +1,14 @@
+import { TodosApp } from "../todo/todo";
+import "../../styles/sideMenu.css";
+
 export default function SideMenu() {
   return (
     <div id="side-menu">
-      <section>Menu 1</section>
-      <section>Menu 2</section>
+      <section>Ami's React App</section>
+      <section>
+        <h2>To learn:</h2>
+        <TodosApp />
+      </section>
     </div>
   );
 }
